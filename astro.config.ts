f2eec7772bc817db39d90a302ec5ef139d3f58e5
paulhmorris <paulh.morris@gmail.com>
2022-09-28 15:44:05 -0500

    Update config

--- astro.config.ts
+++ astro.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-	markdown: {
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
-		},
-	},
-	site: "https://www.astro-theme-cactus.netlify.app",
-	integrations: [
-		mdx({}),
-		tailwind({
-			config: { applyBaseStyles: false },
-		}),
-		image(),
-		sitemap(),
-	],
+  markdown: {
+    shikiConfig: {
+      theme: "nord",
+      wrap: true,
+    },
+  },
+  site: "https://paulmorris.dev",
+  integrations: [
+    mdx({}),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    image(),
+    sitemap(),
+  ],
 });
