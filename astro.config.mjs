ed7eefc13d649055829c54e57b848456863ff11b
paulhmorris <paulh.morris@gmail.com>
2023-11-07 10:33:41 -0600

    feat: deployment, readme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,19 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import fs from "node:fs";
 import rehypePrettyCode from "rehype-pretty-code";
 import { SITE_META } from "./src/consts.ts";
 
 const options = {
   theme: JSON.parse(fs.readFileSync(new URL("./public/darko.json", import.meta.url), "utf-8")),
-  keepBackground: false
+  keepBackground: false,
 };
 
 export default defineConfig({
   site: SITE_META.siteUrl,
   output: "static",
-  integrations: [
-    sitemap(),
-    partytown(),
-    tailwind({ applyBaseStyles: false }),
-    icon({ include: { tabler: ["*"] } }),
-    mdx(),
-  ],
+  integrations: [sitemap(), partytown(), tailwind({ applyBaseStyles: false }), icon({ include: { tabler: ["*"] } }), mdx()],
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [[rehypePrettyCode, options]],
